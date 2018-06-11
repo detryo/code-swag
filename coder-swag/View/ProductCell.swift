9c1f1e6eab3f8ec903ecbf15264fb91ca3256456
Cristian Sedano <cristiansedanoarenas@MacBook-Pro-de-Cristian.local>
2018-06-11 15:40:52 +0100

    Collection Views
    Create new View Controller, create Label UIImage,Collection View, ProductCell.swift

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Cristian Sedano on 5/6/18.
+//  Copyright © 2018 Cristian Sedano. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
